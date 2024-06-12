683941acc4502f038516504b6a5430ed832749c5
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-13 00:31:58 +0200

    Garage: Working create Parking Places

--- Garage/Garage/Garage.cs
+++ Garage/Garage/Garage.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Garage.Vehicles;
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: InternalsVisibleTo("Garage.Test")]
 namespace Garage.Garage
 {
-    internal class Garage
+    internal class Garage<T> : IGarage<T>
+         where T : IParkingPlace //, new()
     {
+        private T[] ParkingPlaces;
+
+        public Garage(T[] parkingPlaces)
+        {
+            ParkingPlaces = parkingPlaces;
+            //for (int i = 0; i < ParkingPlaces.Length; i++)
+            //{
+            //    ParkingPlaces[i] = new T();
+            //}
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var parkingPlace in ParkingPlaces)
+            {
+                yield return parkingPlace;
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
---
+++ Garage/Garage/IGarage.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Garage.Test")]
+namespace Garage.Garage
+{
+    internal interface IGarage<T>: IEnumerable<T>
+        where T : IParkingPlace
+    {
+    }
+}
