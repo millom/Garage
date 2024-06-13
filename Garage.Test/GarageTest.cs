0259696a91e65db924ed1390d99e3f0dae0ba5e4
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-13 10:10:18 +0200

    Garage test: Working tests for VehicleAt and FreeAt

--- Garage.Test/GarageTest.cs
+++ Garage.Test/GarageTest.cs
@@ -27,7 +27,7 @@ namespace Garage.Test
         [InlineData(9)]
         [InlineData(14)]
         [InlineData(19)]
-        public void GivenEmptyGarage_WhenCheckingAnySlot_GivesSlotIsFree(int id)
+        public void GivenEmptyGarage_WhenExecuteFreeAtWithLegalId_ThenSlotIsFree(int id)
         {
             // Act & Assert
             Assert.True(Garage.FreeAt(id));
@@ -36,13 +36,40 @@ namespace Garage.Test
         [Theory]
         [InlineData(-1)]
         [InlineData(20)]
-        public void GivenEmptyGarage_WhenCheckingSlotNotInGarage_GivesException(int id)
+        public void GivenEmptyGarage_WhenExecuteFreeAtWithBadId_ThenThrowExpectedExecpten(int id)
         {
             // Arrange
             var expectedMessage = $"Not existing id <{id}>";
 
             // Act & Assert
             var ex = Assert.Throws<IndexOutOfRangeException>(() => Garage.FreeAt(id));
+
+            // Assert
+            Assert.Equal(expectedMessage, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(4)]
+        [InlineData(9)]
+        [InlineData(14)]
+        [InlineData(19)]
+        public void GivenEmptyGarage_WhenExecuteVehicleAtWithId_GivesSlotIsNull(int id)
+        {
+            // Act & Assert
+            Assert.Null(Garage.VehicleAt(id));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(20)]
+        public void GivenEmptyGarage_WhenExecuteVehicleAtWithBadId_ThenThrowExpectedExecpten(int id)
+        {
+            // Arrange
+            var expectedMessage = $"Not existing id <{id}>";
+
+            // Act & Assert
+            var ex = Assert.Throws<IndexOutOfRangeException>(() => Garage.VehicleAt(id));
 
             // Assert
             Assert.Equal(expectedMessage, ex.Message);
