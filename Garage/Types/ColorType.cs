c7b15922423591ab2c9121dfdecbc4e8948d1725
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-14 01:29:16 +0200

    Add Enum / Type for Color

---
+++ Garage/Types/ColorType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Types
+{
+    internal enum ColorType
+    {
+        BLUE    = 0,
+        YELLOW  = 1,
+        RED     = 2,
+        GREEN   = 3,
+        WHITE   = 4,
+    }
+}
