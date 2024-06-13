ea2af5cbdda3234be992832bd0604110ab9495f8
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-13 18:38:48 +0200

    Add some examples using appsettings

--- Garage/Program.cs
+++ Garage/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Garage.Garage;
+using Garage.Vehicles;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using System;
+
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(Environment.CurrentDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+//var test = config.GetSection("garage:size").Value;
+//var test = config.GetSection("garage:size").GetChildren();
+//Console.WriteLine();
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<IGarage<IVehicle>, Garage<IVehicle>>();
+        services.AddSingleton<IConfiguration>(config);
+    })
+    .UseConsoleLifetime()
+    .Build();
+
+host.Services.GetRequiredService<IGarage<IVehicle>>(); //.Runt()
