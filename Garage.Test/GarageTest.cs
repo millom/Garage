1c7793961204193faaabd87cbfd87c08bf38e7df
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-14 15:49:30 +0200

    Test: Add some tests for parking vehicle

--- Garage.Test/GarageTest.cs
+++ Garage.Test/GarageTest.cs
@@ -1,4 +1,5 @@
 using Garage;
+using Garage.Exceptions;
 using Garage.Garage;
 using Garage.Types;
 using Garage.Vehicles;
@@ -77,7 +78,7 @@ namespace Garage.Test
         }
 
         [Fact]
-        public void ParkCar()
+        public void GivenTwoCarsInGarage_WhenIterateOverGarage_ThenExpectedCarsReached()
         {
             // Arrange
             IVehicle car1 = new Car("ABC123", ColorType.BLUE, 4);
@@ -93,6 +94,76 @@ namespace Garage.Test
             Assert.Equal(car2, enumerator.Current);
             enumerator.MoveNext();
             Assert.Null(enumerator.Current);
+        }
+
+        [Fact]
+        public void GivenOneCarOutsideGarage_WhenParkingLegalCarAndSlotGarage_ThenCarIsParked()
+        {
+            // Arrange
+            IVehicle car = new Car("ABC123", ColorType.BLUE, 4);
+
+            // Act
+            Garage.ParkVehicleInSlot(car, 0);
+
+            Assert.False(Garage.FreeAt(0));
+            Assert.Same(car, Garage.VehicleAt(0));
+        }
+
+        [Fact]
+        public void GivenOneCarOutsideGarage_WhenParkingNullCar_ThenThrowExpectedException()
+        {
+            // Arrange
+            string userDefinedMessage = "vehicle";
+            string expectedMessage = "Value cannot be null." +
+                $" (Parameter '{userDefinedMessage}')";
+            IVehicle? car = null;
+
+            // Act & Assert
+            ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => Garage.ParkVehicleInSlot(car, 0));
+
+            // Assert
+            Assert.Equal(expectedMessage, ex.Message);
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(SIZE)]
+        public void GivenOneCarOutsideGarage_WhenParkingLegalCarOutsideSlots_ThenThrowExpectedException(
+            int slotId)
+        {
+            // Arrange
+            string userDefinedMessage = $"slot={slotId}, range {0}-{SIZE - 1}";
+            string expectedMessage = "Specified argument was out of the range of valid values." +
+                $" (Parameter '{userDefinedMessage}')";
+            IVehicle? car = new Car("ABC123", ColorType.BLUE, 4);
+
+            // Act & Assert
+            ArgumentOutOfRangeException ex = Assert.Throws<ArgumentOutOfRangeException>(
+                () => Garage.ParkVehicleInSlot(car, slotId)
+            );
+
+            // Assert
+            Assert.Equal(expectedMessage, ex.Message);
+        }
+
+        [Fact]
+        public void GivenOneCarOutsideGarage_WhenAddingCarToTakenParkingSlot_ThenThrowExpectedException()
+        {
+            // Arrange
+            int slotId = 0;
+            string expectedMessage =
+                $"Fail to add vehicle to slot {slotId}, place taken";
+            IVehicle car1 = new Car("ABC123", ColorType.BLUE, 4);
+            IVehicle car2 = new Car("ABC124", ColorType.YELLOW, 4);
+            Garage.ParkVehicleInSlot(car1, slotId);
+
+            // Act & Assert
+            SlotTakenException ex = Assert.Throws<SlotTakenException>(
+                () => Garage.ParkVehicleInSlot(car2, slotId)
+            );
+
+            // Assert
+            Assert.Equal(expectedMessage, ex.Message);
         }
     }
 }
