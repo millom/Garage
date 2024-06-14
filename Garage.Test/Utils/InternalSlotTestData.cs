591acec9f1eea91d8da0cf53aa036537033f9684
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-14 15:49:58 +0200

    Test: Add class for Internal test cases

---
+++ Garage.Test/Utils/InternalSlotTestData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Test.Utils
+{
+    public class InternalSlotTestData
+    {
+        private static readonly List<object[]> Data =
+        [
+            new object[] { 0 },
+            new object[] { 4 },
+            new object[] { 9 },
+            new object[] { 14 },
+            new object[] { 19 }
+        ];
+
+        public static IEnumerable<object[]> TestData => Data;
+    }
+}
