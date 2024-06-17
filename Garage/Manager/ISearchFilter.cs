e96661889a7c73b7372a8653343ad6527cc9826c
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-17 20:30:07 +0200

    Manager: Add SearchFilter

---
+++ Garage/Manager/ISearchFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace Garage.Manager
+{
+    internal interface ISearchFilter
+    {
+        int? Color { get; set; }
+        int? ExtraProp { get; set; }
+        string? RegNumber { get; set; }
+        int? Weels { get; set; }
+        PropertyInfo[] GetPublicInstanceProps();
+    }
+}
---
+++ Garage/Manager/SearchFilter.cs
@@ -0,0 +1,34 @@
+﻿using Garage.Types;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Garage.Manager
+{
+    internal class SearchFilter : ISearchFilter
+    {
+        public string? RegNumber { get; set; }
+        public int? Color { get; set; }
+        public int? Weels { get; set; }
+        public int? ExtraProp { get; set; }
+
+        public PropertyInfo[] GetPublicInstanceProps()
+        {
+            return this.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
+        }
+
+        public override string ToString()
+        {
+            return
+                $"RegNr:      {(RegNumber != null ? RegNumber : "---")}\n" +
+                $"Color:      {(Color != null ? Color : "---")}\n" +
+                $"Weels:      {(Weels != null ? Weels : "---")}\n" +
+                $"ExtraProp:  {(ExtraProp != null ? ExtraProp : "---")}";
+        }
+    }
+}
