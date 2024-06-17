4f87e3955e4103d09ed70d08c09feb9e17b810b6
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-17 12:20:13 +0200

    Test: Add Base class for Vehicle tests

---
+++ Garage.Test/Tests/Vehicles/BaseVehicleTest.cs
@@ -0,0 +1,17 @@
+﻿using Garage.Types;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Test.Tests.Vehicles
+{
+    public class BaseVehicleTest
+    {
+        protected const string regNumber = "ABC123";
+        protected const ColorType color = ColorType.BLUE;
+        protected const int weels = 4;
+    }
+}
