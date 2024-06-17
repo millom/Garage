4ba28a91b3333138150c580b7e2cfab332e9a9d7
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-17 20:40:10 +0200

    Manager: Work with the menu, WIP

--- Garage/Manager/Manager.cs
+++ Garage/Manager/Manager.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Garage.Garage;
+using Garage.Types;
+using Garage.UI;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,141 @@ namespace Garage.Manager
 {
     internal class Manager
     {
+        private readonly IUI _ui;
+        private readonly IGarageHandler _garageHandler;
+        private readonly ISearchFilter _searchFilter;
+
+        public Manager(
+            IUI ui,
+            IGarageHandler garageHandler,
+            ISearchFilter searchFilter)
+        {
+            _ui = ui;
+            _garageHandler = garageHandler;
+            _searchFilter = searchFilter;
+        }
+
+        public void Run()
+        {
+            while (MainMenu());
+        }
+
+        public bool MainMenu()
+        {
+            _ui.Clear();
+            _ui.WriteLine("--- MAIN MENU ---");
+            _ui.WriteLine("0: Park vehicle");
+            _ui.WriteLine("1: Get parked vehicle");
+            _ui.WriteLine("2: Show parked vehicles");
+            _ui.WriteLine("9: Exit program");
+
+            var command = _ui.ReadLine();
+
+            switch (command)
+            {
+                case "0":
+                    while(ParkMenu());
+                    break;
+                case "1":
+                    while(UnparkMenu());
+                    break;
+                case "2":
+                    while(ShowParkedVehiclesMenu());
+                    break;
+                default:
+                    break;
+            }
+
+            return command != "9";
+        }
+
+        public bool ParkMenu()
+        {
+            _ui.Clear();
+            _ui.WriteLine("--- PARK VEHICLE MENU --");
+
+            var command = _ui.ReadLine();
+
+            return command != "9";
+        }
+
+        public bool UnparkMenu()
+        {
+            _ui.Clear();
+            _ui.WriteLine("--- PARK VEHICLE MENU --");
+
+            var command = _ui.ReadLine();
+
+            return command != "9";
+        }
+
+        public bool ShowParkedVehiclesMenu()
+        {
+            _ui.Clear();
+            _ui.WriteLine("--- SEARCH PARKED VEHICLES MENU --");
+            _ui.WriteLine("0: Do search");
+            _ui.WriteLine($"Search Params");
+            _ui.WriteLine(_searchFilter.ToString()!);
+            _ui.WriteLine("1: Set RegNumber param");
+            _ui.WriteLine("2: Set Color param");
+            _ui.WriteLine("3: Set Weels param");
+            _ui.WriteLine("4: Set ExtraProp param");
+            _ui.WriteLine("9: Exit menu");
+
+            var command = _ui.ReadLine();
+
+            switch (command)
+            {
+                case "0":
+                    PrintSearchResult();
+                    break;
+                case "1":
+                    SetRegNumber();
+                    break;
+                case "2":
+                    //SetColor();
+                    break;
+                case "3":
+                    //SetWeels();
+                    break;
+                case "4":
+                    //SetExtraProp();
+                    break;
+            }
+
+            return _ui.ReadLine() != "9";
+        }
+
+        private void SetRegNumber()
+        {
+            _searchFilter.GetPublicInstanceProps();
+            foreach (var prop in _searchFilter.GetPublicInstanceProps())
+            {
+                _ui.WriteLine($"{prop.Name}");
+            }
+        }
+
+        private void PrintSearchResult()
+        {
+            foreach (var vehicle in _garageHandler.GetSearchResult(_searchFilter))
+            {
+                _ui.WriteLine(vehicle);
+            };
+        }
+
+        public bool SearchParkedMenu()
+        {
+            _ui.Clear();
+        }
+
+        public void PrintNotParkedMenu()
+        {
+
+        }
+
+        public void PrintSeeParked()
+        {
+
+        }
     }
 }
