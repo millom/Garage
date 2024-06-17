ce053140de3987dc9498e4a8f2bdf80a84ea0f02
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-17 14:35:31 +0200

    Exceptions: Add Exceptions heöper

---
+++ Garage/Exceptions/Throw.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.Exceptions
+{
+    internal class Throw<T> where T : Exception
+    {
+        public static void If(bool isValid, string message)
+        {
+            if (!isValid) return;
+
+            Exception? e = Activator.CreateInstance(typeof(T), message) as Exception;
+            if (e != null) throw e;
+
+            throw Activator.CreateInstance<T>();
+        }
+    }
+}
