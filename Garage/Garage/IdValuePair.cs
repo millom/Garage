f2f775a7e6989047a5f83c3177334961df97e73e
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-19 10:07:02 +0200

    IdValuePair: Add class for JSon, serialize save

---
+++ Garage/Garage/IdValuePair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Garage.Garage
+{
+    internal class IdValuePair<T>(int id, T value) where T: class
+    {
+        [JsonConstructor]
+        public IdValuePair() : this(0, null!) {}
+
+        [JsonPropertyOrder(1)]
+        public int Id { get; } = id;
+
+        [JsonPropertyOrder(2)]
+        public T Value { get; } = value;
+    }
+}
