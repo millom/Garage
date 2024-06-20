8eaccd6fd6b6f914fc767030e47ca42bd543fba2
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-06-20 18:12:24 +0200

    IUI: Add comments

--- Garage/UI/IUI.cs
+++ Garage/UI/IUI.cs
@@ -8,9 +8,26 @@ namespace Garage.UI
     /// </summary>
     public interface IUI
     {
+        /// <summary>
+        /// Define method like Console.Write
+        /// </summary>
+        /// <param name="line"></param>
         void Write(string line);
+
+        /// <summary>
+        /// Define method like Console.WriteLine
+        /// </summary>
+        /// <param name="line"></param>
         void WriteLine(string line);
+
+        /// <summary>
+        /// Define method like Console.ReadLine
+        /// </summary>
         string? ReadLine();
+
+        /// <summary>
+        /// Define method like Console.Clear
+        /// </summary>
         void Clear();
     }
 }
